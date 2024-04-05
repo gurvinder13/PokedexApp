03028e90b47f699af777b1730df79a7999d51e71
Gurvinder Singh <gurvinderscs@gmail.com>
2024-04-05 16:56:45 +0530

    List screen is updated

--- app/src/main/java/com/example/pokedexapp/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokedexapp/pokemonlist/PokemonListScreen.kt
@@ -2,33 +2,62 @@ package com.example.pokedexapp.pokemonlist
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.compose.ImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
 import com.example.pokedexapp.R
+import com.example.pokedexapp.data.models.PokemonListEntry
+import com.example.pokedexapp.ui.theme.RobotoCondensed
 
 @Composable
 fun PokemonListScreen(
@@ -49,8 +78,7 @@ fun PokemonListScreen(
                     .align(Alignment.CenterHorizontally)
             )
             Spacer(
-                modifier = Modifier
-                    .height(16.dp)
+                modifier = Modifier.height(16.dp)
             )
             SearchBar(
                 hint = "Search...", modifier = Modifier
@@ -59,6 +87,10 @@ fun PokemonListScreen(
             ) {
 
             }
+            Spacer(
+                modifier = Modifier.height(16.dp)
+            )
+            PokemonList(navController = navController)
         }
     }
 }
@@ -104,4 +136,183 @@ fun SearchBar(
         }
 
     }
+}
+
+@Composable
+fun PokemonList(
+    navController: NavController, viewModel: PokemonListViewModel = hiltViewModel()
+) {
+    val pokemonList by remember { viewModel.pokemonList }
+    val endReached by remember { viewModel.endReached }
+    val loadError by remember { viewModel.loadError }
+    val isLoading by remember { viewModel.isLoading }
+
+    LazyColumn(contentPadding = PaddingValues(16.dp)) {
+        val itemCount = if (pokemonList.size % 2 == 0) {
+            pokemonList.size / 2
+        } else {
+            pokemonList.size / 2 + 1
+        }
+        items(itemCount) {
+            if (it >= itemCount - 1 && !endReached) {
+                viewModel.loadPokemonPaginated()
+            }
+            PokemonRow(rowIndex = it, entries = pokemonList, navController = navController)
+        }
+    }
+
+}
+
+/*@Composable
+fun PokemonEntry(
+    entry: PokemonListEntry,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    viewModel: PokemonListViewModel = hiltViewModel()
+) {
+    val defaultDominantColor = MaterialTheme.colors.surface
+    var dominantColor by remember {
+        mutableStateOf(defaultDominantColor)
+    }
+
+    Box(contentAlignment = Alignment.Center,
+        modifier = modifier
+            .shadow(5.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .aspectRatio(1f)
+            .background(
+                Brush.verticalGradient(
+                    listOf(
+                        dominantColor, defaultDominantColor
+                    )
+                )
+            )
+            .clickable {
+                navController.navigate(
+                    "pokemon_detail_screen/${dominantColor.toArgb()}/${entry.pokemonName}"
+                )
+            }) {
+        Column {
+            SubcomposeAsyncImage(
+                model = entry.imageUrl,
+                contentDescription = entry.pokemonName,
+                loading = {
+                    CircularProgressIndicator(
+                        color = MaterialTheme.colors.primary, modifier = Modifier.scale(0.5f)
+                    )
+                },
+                success = { success ->
+                    viewModel.calcDominantColor(success.result.drawable) {
+                        // Update the dominant color state variable
+                        dominantColor = it
+                    }
+                },
+                *//*success = { success ->
+                    viewModel.calcDominantColor(success.result.drawable) {
+                        dominantColor = it
+                    }
+                },*//*
+
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(CenterHorizontally)
+            )
+            Text(
+                text = entry.pokemonName,
+                fontFamily = RobotoCondensed,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                modifier = modifier.fillMaxWidth()
+            )
+        }
+    }
+}*/
+
+@Composable
+fun PokemonEntry(
+    entry: PokemonListEntry,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    viewModel: PokemonListViewModel = hiltViewModel()
+) {
+    val defaultDominantColor = MaterialTheme.colors.surface
+    var dominantColor by remember { mutableStateOf(defaultDominantColor) }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .shadow(5.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .aspectRatio(1f)
+            .background(
+                Brush.verticalGradient(
+                    listOf(
+                        Color.Transparent, defaultDominantColor
+                    )
+                )
+            )
+            .clickable {
+                navController.navigate(
+                    "pokemon_detail_screen/${dominantColor.toArgb()}/${entry.pokemonName}"
+                )
+            }
+    ) {
+        Column {
+            SubcomposeAsyncImage(
+                model = entry.imageUrl,
+                contentDescription = entry.pokemonName,
+                loading = {
+                    CircularProgressIndicator(
+                        color = MaterialTheme.colors.primary,
+                        modifier = Modifier.scale(0.5f)
+                    )
+                },
+               /* success = { success ->
+                    viewModel.calcDominantColor(success.result.drawable) {
+                        dominantColor = it
+                    }
+                },*/
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(CenterHorizontally)
+            )
+            Text(
+                text = entry.pokemonName,
+                fontFamily = RobotoCondensed,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                modifier = modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+
+
+@Composable
+fun PokemonRow(
+    rowIndex: Int, entries: List<PokemonListEntry>, navController: NavController
+) {
+    Column {
+        Row {
+            PokemonEntry(
+                entry = entries[rowIndex * 2 + 1],
+                navController = navController,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            if (entries.size >= rowIndex * 2 + 2) {
+                PokemonEntry(
+                    entry = entries[rowIndex * 2],
+                    navController = navController,
+                    modifier = Modifier.weight(1f)
+                )
+            } else {
+                Spacer(modifier = Modifier.weight(1f))
+            }
+
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+
 }
